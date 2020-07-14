0a5b50e864269153331bee77d1dbe832af0bfa81
5HT <maxim@synrc.com>
2020-07-14 14:57:43 +0300

    fsproj

--- firestarter.sh
+++ firestarter.sh
@@ -2,7 +2,7 @@
 
 rm -rf bin
 rm -rf obj
-dotnet build docx.sln
+dotnet build docx.fsproj
 cp Logo.jpg in.docx sample.csv bin/Debug/netcoreapp3.1
 cd bin/Debug/netcoreapp3.1
 ./docx -vars vector "Table" "sample.csv" , \
